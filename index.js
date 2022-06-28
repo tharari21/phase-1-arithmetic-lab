4dce7b2f93cb2b424b48956130b90ff7591276cd
Tomer Harari <tharari21@gmail.com>
2022-06-28 10:08:52 -0400

    commit tree 807569e6127ab0f42fa6ba4a146ce975a3b4750f

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100);
+let num3 = 104;
+let num4 = 25;
+const mod = num3 % num4;
+
+const max = Math.max(14, 6, 10, 20);
